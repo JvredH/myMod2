8a97d1621331f408c490dec8575eaa8e8c6eabb8
jared <hem.jared@gmail.com>
2022-10-26 22:34:50 -0700

    day 3

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
@@ -8,11 +8,14 @@ class DoublyLinkedListNode {
 
 class DoublyLinkedList {
   constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
     // Your code here
   }
 
   addToHead(val) {
-    // Your code here
+    
   }
 
   addToTail(val) {
--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
@@ -7,15 +7,48 @@ class LinkedListNode {
 
 class LinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.length = 0;
+    this.tail = undefined;
   }
 
   addToHead(val) {
-    // Your code here
+    const newNode = new LinkedListNode(val, null);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++
+    //this.print()
+
   }
 
   addToTail(val) {
-    // Your code here
+    const newNode = new LinkedListNode(val);
+    this.length++;
+
+    //case 1: There are NOT any nodes in the list;
+    if(!this.head) {
+      this.head = newNode;
+      return this;
+    }
+    // case 2: There ARE nodes in the list;
+    let currentNode = this.head;
+
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = newNode;
+    // if (this.head === null) {
+    //   this.head = newNode(val);
+    //   return;
+    // }
+
+    // let curr = this.head;
+    // while (curr.next !== null) {
+    //   curr = curr.next;
+    // }
+    // curr.next = new Node(val);
   }
 
   // You can use this function to help debug
